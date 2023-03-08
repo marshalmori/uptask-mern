aabf386fcde103ab830877c4bcd50a04a9334cd5
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-07 21:56:53 -0300

    Enviar request, ler dados e criar usuários

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,3 +1,15 @@
-const registrar = (req, res) => {};
+import Usuario from "../models/Usuario.js";
+
+const registrar = async (req, res) => {
+  try {
+    const usuario = new Usuario(req.body);
+    const usuarioAlmacenado = await usuario.save();
+    res.json(usuarioAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.json({ msg: "Criar usuário" });
+};
 
 export { registrar };
