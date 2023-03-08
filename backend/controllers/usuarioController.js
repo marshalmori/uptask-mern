3f07d9f9a6ac55cc9a37dbb89e29bae0773c4311
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-07 22:08:02 -0300

    Previnir usuários duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,14 @@
 import Usuario from "../models/Usuario.js";
 
 const registrar = async (req, res) => {
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email });
+
+  if (existeUsuario) {
+    const error = new Error("Usuário já cadastrado.");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const usuario = new Usuario(req.body);
     const usuarioAlmacenado = await usuario.save();
