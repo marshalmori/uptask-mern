20db37a2ff188229feca0f4e1b688908c7cad2ac
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-08 16:47:03 -0300

    Iniciar a funcionalidade esqueceu senha

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -74,4 +74,22 @@ const confirmar = async (req, res) => {
   }
 };
 
-export { registrar, autenticar, confirmar };
+const olvidePassword = async (req, res) => {
+  const { email } = req.body;
+  const usuario = await Usuario.findOne({ email });
+
+  if (!usuario) {
+    const error = new Error("El Usuario no existe");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    usuario.token = generarId();
+    await usuario.save();
+    res.json({ msg: "Hemos enviado un email con las instrucciones" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { registrar, autenticar, confirmar, olvidePassword };
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -5,11 +5,13 @@ import {
   registrar,
   autenticar,
   confirmar,
+  olvidePassword,
 } from "../controllers/usuarioController.js";
 
 // Autenticación, Registro y Confirmación de Usuarios
 router.post("/", registrar);
 router.post("/login", autenticar);
 router.get("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword); // esqueceu senha
 
 export default router;
