070fa860c5f0ea57bba90f18ede312e15bce5085
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-08 17:11:10 -0300

    Armazenar a nova senha - esqueceu senha

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -7,6 +7,7 @@ import {
   confirmar,
   olvidePassword,
   comprobarToken,
+  nuevoPassword,
 } from "../controllers/usuarioController.js";
 
 // Autenticación, Registro y Confirmación de Usuarios
@@ -14,6 +15,6 @@ router.post("/", registrar);
 router.post("/login", autenticar);
 router.get("/confirmar/:token", confirmar);
 router.post("/olvide-password", olvidePassword); // esqueceu senha
-router.get("/olvide-password/:token", comprobarToken);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
 export default router;
