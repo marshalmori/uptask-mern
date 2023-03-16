3c1e029699435c58d9a697b61fe75e9a97665e96
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-16 13:52:36 -0300

    Instalar e configurar NodeMailer para envio de confirmação de criação de conta

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,7 @@
 import Usuario from "../models/Usuario.js";
 import generarId from "../helpers/generarId.js";
 import generarJWT from "../helpers/generarJWT.js";
+import { emailRegistro } from "../helpers/email.js";
 
 const registrar = async (req, res) => {
   const { email } = req.body;
@@ -15,6 +16,14 @@ const registrar = async (req, res) => {
     const usuario = new Usuario(req.body);
     usuario.token = generarId();
     await usuario.save();
+
+    //Enviar el email de confirmacion
+    emailRegistro({
+      email: usuario.email,
+      nombre: usuario.nombre,
+      token: usuario.token,
+    });
+
     res.json({
       msg: "Usuario Creado Correctamente. Revisa tu Email para confirmar tu cuenta.",
     });
