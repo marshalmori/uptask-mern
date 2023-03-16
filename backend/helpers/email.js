9851e7029a4c213085a33b0c64098c172b584a43
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-16 14:11:18 -0300

    Enviar email de confirmação de criação de conta

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -1,5 +1,29 @@
 import nodemailer from "nodemailer";
 
-export const emailRegistro = (datos) => {
-  console.log(datos);
+export const emailRegistro = async (datos) => {
+  const { email, nombre, token } = datos;
+
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "285df46dc037c0",
+      pass: "fc0125e3b362d6",
+    },
+  });
+
+  //Información del email
+  const info = await transport.sendMail({
+    from: ' "UpTask - Administrador de Proyectos" <cuentas@uptask.com> ',
+    to: email,
+    subject: "UpTask - Comprueba tu cuenta",
+    text: "Comprueba tu cuenta en UpTask",
+    html: `<p>Hola: ${nombre} Comprueba tu cuenta em UpTask</p>
+    <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace:
+
+    <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
+
+    <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+    `,
+  });
 };
