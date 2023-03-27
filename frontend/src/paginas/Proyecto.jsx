201645b16a64eb96e849e2e49ce6c1eb6d0a6667
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-27 15:01:58 -0300

    Apresentar Modal quando clicar no botão de Nova tarefa

--- frontend/src/paginas/Proyecto.jsx
+++ frontend/src/paginas/Proyecto.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useProyectos from "../hooks/useProyectos";
 import { useParams, Link } from "react-router-dom";
 import ModalFormularioTarea from "../components/ModalFormularioTarea";
@@ -6,6 +6,8 @@ import ModalFormularioTarea from "../components/ModalFormularioTarea";
 const Proyecto = () => {
   const params = useParams();
   const { obtenerProyecto, proyecto, cargando } = useProyectos();
+
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     obtenerProyecto(params.id);
@@ -45,6 +47,7 @@ const Proyecto = () => {
       </div>
 
       <button
+        onClick={() => setModal(true)}
         type="button"
         className=" flex gap-2 text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 items-center justify-center"
       >
@@ -63,7 +66,7 @@ const Proyecto = () => {
         Nueva Tarea
       </button>
 
-      <ModalFormularioTarea />
+      <ModalFormularioTarea modal={modal} setModal={setModal} />
     </>
   );
 };
