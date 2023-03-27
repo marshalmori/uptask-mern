517f99c6d24061343e24198d5373435e6afde9ef
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-27 17:30:46 -0300

    Mostrar as tarefas de um projeto

--- frontend/src/paginas/Proyecto.jsx
+++ frontend/src/paginas/Proyecto.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useProyectos from "../hooks/useProyectos";
 import { useParams, Link } from "react-router-dom";
 import ModalFormularioTarea from "../components/ModalFormularioTarea";
+import Tarea from "../components/Tarea";
 
 const Proyecto = () => {
   const params = useParams();
@@ -66,6 +67,20 @@ const Proyecto = () => {
         </svg>
         Nueva Tarea
       </button>
+
+      <p className="font-bold text-xl mt-10">Tareas del Proyecto</p>
+
+      <div className="bg-white shadow mt-10 rounded-lg">
+        {proyecto.tareas?.length ? (
+          proyecto.tareas?.map((tarea) => (
+            <Tarea key={tarea._id} tarea={tarea} />
+          ))
+        ) : (
+          <p className="text-center my-5 p-10">
+            No hay tareas en este proyecto
+          </p>
+        )}
+      </div>
 
       <ModalFormularioTarea modal={modal} setModal={setModal} />
     </>
