1597e5e20e6713807bbc8ba26a2068120316d89e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-27 15:53:16 -0300

    Criar o formulário para criar uma tarefa

--- frontend/src/components/ModalFormularioTarea.jsx
+++ frontend/src/components/ModalFormularioTarea.jsx
@@ -2,7 +2,13 @@ import { Fragment, useState, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import useProyectos from "../hooks/useProyectos";
 
+const PRIORIDAD = ["Baja", "Media", "Alta"];
+
 const ModalFormularioTarea = () => {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [prioridad, setPrioridad] = useState("");
+
   const { modalFormularioTarea, handleModalTarea } = useProyectos();
 
   return (
@@ -71,10 +77,68 @@ const ModalFormularioTarea = () => {
                     as="h3"
                     className="text-lg leading-6 font-bold text-gray-900"
                   >
-                    <h1 className="text-4xl">Titulo</h1>
+                    Crear Tarea
                   </Dialog.Title>
+                  <form className="my-10">
+                    <div className="mb-5">
+                      <label
+                        className="text-gray-700 uppercase font-bold text-sm"
+                        htmlFor="nombre"
+                      >
+                        Nombre Tarea
+                      </label>
+                      <input
+                        type="text"
+                        id="nombre"
+                        placeholder="Nombre de la tarea"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                      />
+                    </div>
 
-                  <p>Contenido</p>
+                    <div className="mb-5">
+                      <label
+                        className="text-gray-700 uppercase font-bold text-sm"
+                        htmlFor="descripcion"
+                      >
+                        Descripcion Tarea
+                      </label>
+                      <textarea
+                        id="descripcion"
+                        placeholder="Descripción de la tarea"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={descripcion}
+                        onChange={(e) => setDescripcion(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="mb-5">
+                      <label
+                        className="text-gray-700 uppercase font-bold text-sm"
+                        htmlFor="prioridad"
+                      >
+                        Prioridad
+                      </label>
+                      <select
+                        id="nombre"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={prioridad}
+                        onChange={(e) => setPrioridad(e.target.value)}
+                      >
+                        <option value="">-- Seleccionar Prioridad --</option>
+                        {PRIORIDAD.map((opcion) => (
+                          <option key={opcion}>{opcion}</option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <input
+                      type="submit"
+                      className="bg-sky-600 hover:bg-sky-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors rounded-lg text-sm"
+                      value="Crear Tarea"
+                    />
+                  </form>
                 </div>
               </div>
             </div>
