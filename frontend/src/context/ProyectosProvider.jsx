a446bed6d980fb4a6c164ab210208c3ec0966275
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-03 14:31:43 -0300

    Criar as validações para poder inserir um colaborador

--- frontend/src/context/ProyectosProvider.jsx
+++ frontend/src/context/ProyectosProvider.jsx
@@ -138,7 +138,10 @@ const ProyectosProvider = ({ children }) => {
       const { data } = await clienteAxios(`/proyectos/${id}`, config);
       setProyecto(data);
     } catch (error) {
-      console.log(error);
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
     } finally {
       setCargando(false);
     }
@@ -323,7 +326,36 @@ const ProyectosProvider = ({ children }) => {
   };
 
   const agregarColaborador = async (email) => {
-    console.log(email);
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await clienteAxios.post(
+        `/proyectos/colaboradores/${proyecto._id}`,
+        email,
+        config
+      );
+
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+
+      setColaborador({});
+      setAlerta({});
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
   };
 
   return (
--- frontend/src/paginas/NuevoColaborador.jsx
+++ frontend/src/paginas/NuevoColaborador.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import FormularioColaborador from "../components/FormularioColaborador";
 import useProyectos from "../hooks/useProyectos";
 import { useParams } from "react-router-dom";
+import Alerta from "../components/Alerta";
 
 const NuevoColaborador = () => {
   const {
@@ -10,6 +11,7 @@ const NuevoColaborador = () => {
     cargando,
     colaborador,
     agregarColaborador,
+    alerta,
   } = useProyectos();
   const params = useParams();
 
@@ -17,7 +19,9 @@ const NuevoColaborador = () => {
     obtenerProyecto(params.id);
   }, []);
 
-  if (cargando) return "cargando...";
+  // if (cargando) return "cargando...";
+
+  if (!proyecto?._id) return <Alerta alerta={alerta} />;
 
   return (
     <>
